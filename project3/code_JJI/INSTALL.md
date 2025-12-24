29df7153299bb7d8b5c0111295391e0c46d6007d
JulianSierraRodriguez <julian.sierra.rodriguez@gmail.com>
2025-12-24 17:33:09 +0100

    Install.md and some corrections to the Makefile.

---
+++ project3/code_JJI/INSTALL.md
@@ -0,0 +1,30 @@
+
+Installation Guide
+
+1. Compile the Code
+
+Open the terminal and in the directory containing the Makefile run:
+
+make
+
+This will build the module, object fils in the build folder and the executable file "program.exe" in the directory you are currently in.
+
+2. Run Tests [Optional]
+
+To compile, and execute the tests for this code, use:
+
+make test
+
+
+3. Run the Program
+
+After the compilation, execute the program with:
+
+./program.exe
+
+
+4. Makefile [Optional]
+
+To see what single steps can be done with the Makefile, use:
+
+make help
--- project3/code_JJI/src/main.f90
+++ project3/code_JJI/src/main.f90
@@ -27,12 +27,12 @@ program main
   epsilon   = 0.997 ! kJ/mol
   sigma     = 3.405 ! angstrom
   sigma = sigma / 10 ! nm
-  time_step = 0.002; steps     = 1000;  M         = 10
+  time_step = 0.002; steps = 1000;  M = 10
 
   ! Outputs parameters
 
-  u_output = 10; u_xyz    = 20
-  output_file = "MD.out"; xyz_file    = "trajectory.xyz"
+  u_output = 10; u_xyz = 20
+  output_file = "MD.out"; xyz_file = "trajectory.xyz"
 
   ! We call the create output (treat_output module) to generate or replace the output and trajectory files.
   call create_output(u_output,output_file)
@@ -42,20 +42,20 @@ program main
   Natoms = read_Natoms(input_file)
 
   ! We call the write_output_head function (treat_output module) to output the initial parameters to the output file.
-  call write_output_head(u_output, input_file, epsilon, sigma, time_step, steps, M, output_file, u_xyz, xyz_file,Natoms)
+  call write_output_head(u_output, input_file, epsilon, sigma, time_step, steps, M, output_file, u_xyz, xyz_file, Natoms)
 
   ! We allocate the arrays for the initial coordinates and the masses, and check if they were correctly allocated using the error_allocate subroutine (error_treat module).
   allocate (coord(Natoms,3), stat=i_stat)
   call error_allocate(i_stat)
 
-  allocate (mass(Natoms),stat=i_stat)
+  allocate (mass(Natoms), stat=i_stat)
   call error_allocate(i_stat)
 
   ! We call the subroutine read_molecule (treat_input module) to obtain the coordinates and masses from the input file.
-  call read_molecule(input_file, Natoms, coord,mass)
+  call read_molecule(input_file, Natoms, coord, mass)
 
   ! We allocate the array for the distances and check if it was correctly allocated.
-  allocate (distance(Natoms,Natoms),stat=i_stat)
+  allocate (distance(Natoms,Natoms), stat=i_stat)
   call error_allocate(i_stat)
 
   ! We call the subroutine compute_distances (treat_input module) to compute the distances between the atoms.
@@ -71,7 +71,7 @@ program main
   V_total = V(epsilon, sigma, Natoms, distance) ! kJ/mol
 
   ! We allocate the velocities array, check it, and then we initialize it to 0.
-  allocate (velocity(Natoms,3),stat=i_stat)
+  allocate (velocity(Natoms,3), stat=i_stat)
   call error_allocate(i_stat)
 
   velocity = 0.0d0
@@ -81,7 +81,7 @@ program main
   T_total = T(Natoms, velocity, mass) ! kJ/mol
 
   ! We use the E function (energies module) to obtain the initial total energy.
-  E_total = E(V_total,T_total) ! kJ/mol
+  E_total = E(V_total, T_total) ! kJ/mol
 
 
   !----------------------------!
